86a158cbe55fe63fd23e31ad53599af27758d315
helen <alnorda@gmail.com>
2021-12-22 18:24:24 +0300

    acync parsec

--- tasks/asinc_parser.py
+++ tasks/asinc_parser.py
@@ -1,7 +1,8 @@
 import asyncio
 import aiohttp
-from bs4 import BeautifulSoup as BS
 from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup as BS
 
 
 async def get_urls_list(base_url):
@@ -20,10 +21,13 @@ async def get_data(url):
         soup = BS(await response.text(), features="html.parser")
         elements = soup.find('tbody').findAll('tr')
         data = [list(map(lambda x: x.text, element.findAll('td'))) for element in elements]
-        print(data)
     await session.close()
+    return data
 
-urls_list = asyncio.run(get_urls_list('http://127.0.0.1:8000/'))
 
-features = [get_data(url) for url in urls_list]
-asyncio.run(asyncio.wait(features))
+async def main():
+    urls_list = await get_urls_list('http://127.0.0.1:8000/')
+    result, pending = await asyncio.wait([get_data(url) for url in urls_list])
+    print(*(item.result() for item in result), sep='\n')
+
+asyncio.run(main())
--- warehouse_project/warehouse/warehouse/settings/dev_settings.py
+++ warehouse_project/warehouse/warehouse/settings/dev_settings.py
@@ -1,26 +1,25 @@
 from .base import *
 SECRET_KEY = 'django-insecure-o02$nzfa1a_glhg3b*5$hq_7r7lfjl1l1nhnk%!^^%#jqnvsfi'
 
-SITE_ID = 2
-
+SITE_ID = 3
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends':
-            {
-                'handlers': ['console'],
-                'level': 'DEBUG',
-            }
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends':
+#             {
+#                 'handlers': ['console'],
+#                 'level': 'DEBUG',
+#             }
+#     },
+# }
